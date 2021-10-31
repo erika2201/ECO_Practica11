1ac370293504de3ae76df54c2bc56a88dd96411d
erika2201 <erikapapamija@gmail.com>
2021-10-30 20:24:07 -0500

    Ya aparecen post de diferentes usuarios

--- src/postcard.js
+++ src/postcard.js
@@ -1,5 +1,6 @@
 //Es como una clase que representa una tarjeta
-//Genera el HTML necesario para visulizar mis posts
+//Genera el HTML necesario para visulizar mis 
+
 
 export class postCard {
 
@@ -10,12 +11,33 @@ export class postCard {
     //metodo para que me devuelva HTML y generar tarjeta
     render(){
         let card = document.createElement("div");
-        card.className = "postCard";
-       
+        card.className = "post-card";
 
+        //Post original (texto)
+        let message = document.createElement("p");
+        message.className = "post-message"
+        message.innerHTML = this.post.text;
+
+        //Input para responder
+        let answer = document.createElement("input");
+        answer.className = "answer-input";
+        answer.placeholder = "Escribe una respuesta";
+
+        //@ del usuario que hizo la publicación
+        let username = document.createElement("p");
+        username.className = "post-username";
+        username.innerHTML = "@" + this.post.nombre;
+
+        //Boton para responder
+        let answerBtn = document.createElement("button");
+        answerBtn.className = "answer-button";
+        answerBtn.innerHTML = "Responder";
+
+       
+        card.appendChild(message);
+        card.appendChild(username);
+        card.appendChild(answer);
+        card.appendChild(answerBtn);
         return card;
     }
-
-
-
 }
